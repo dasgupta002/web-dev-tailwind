396b425b0d982771bcd8a5bfde0550966479f4d0
Subhrakanti Dasgupta <subhrakanti.dasgupta@gmail.com>
2021-05-16 02:19:20 +0530

    Add files via upload

---
+++ Online Startup Landing Page/tailwind.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  purge: [],
+  darkMode: false, 
+  theme: {
+    extend: {
+      colors: {
+        primary: '#1A202C'
+      },
+      fontFamily: {
+        header: ['Shadows Into Light']
+      } 
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
